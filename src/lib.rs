51f369b0c33a1abb51842c68c4cad7731f2ae270
Devon7925 <devonrutledge03@gmail.com>
2025-02-03 15:09:32 -0700

    various reflection improvements

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,16 @@ use std::ffi::{CStr, CString};
 use std::marker::PhantomData;
 use std::ptr::{null, null_mut};
 
-use slang_sys as sys;
+use slang_sys::{self as sys, SlangInt32};
 
 pub use sys::{
-	slang_CompilerOptionName as CompilerOptionName, SlangCompileTarget as CompileTarget,
-	SlangDebugInfoLevel as DebugInfoLevel, SlangFloatingPointMode as FloatingPointMode,
-	SlangLineDirectiveMode as LineDirectiveMode, SlangMatrixLayoutMode as MatrixLayoutMode,
-	SlangOptimizationLevel as OptimizationLevel, SlangParameterCategory as ParameterCategory,
-	SlangSourceLanguage as SourceLanguage, SlangStage as Stage, SlangUUID as UUID,
+	slang_CompilerOptionName as CompilerOptionName, SlangBindingType as BindingType,
+	SlangCompileTarget as CompileTarget, SlangDebugInfoLevel as DebugInfoLevel,
+	SlangFloatingPointMode as FloatingPointMode, SlangLineDirectiveMode as LineDirectiveMode,
+	SlangMatrixLayoutMode as MatrixLayoutMode, SlangOptimizationLevel as OptimizationLevel,
+	SlangParameterCategory as ParameterCategory, SlangResourceShape as ResourceShape,
+	SlangSourceLanguage as SourceLanguage, SlangStage as Stage, SlangTypeKind as TypeKind,
+	SlangUUID as UUID, SlangScalarType as ScalarType
 };
 
 macro_rules! vcall {
@@ -419,6 +421,13 @@ unsafe impl Downcast<ComponentType> for EntryPoint {
 	}
 }
 
+impl EntryPoint {
+	pub fn get_function_reflection(&self) -> &reflection::Function {
+		let ptr = vcall!(self, getFunctionReflection());
+		unsafe { &*(ptr as *const _) }
+	}
+}
+
 #[repr(transparent)]
 #[derive(Clone)]
 pub struct TypeConformance(IUnknown);
@@ -464,6 +473,18 @@ impl Module {
 		let name = CString::new(name).unwrap();
 		let mut entry_point = null_mut();
 		vcall!(self, findEntryPointByName(name.as_ptr(), &mut entry_point));
+		Some(EntryPoint(IUnknown(std::ptr::NonNull::new(
+			entry_point as *mut _,
+		)?)))
+	}
+
+	pub fn get_defined_entry_point_count(&self) -> SlangInt32 {
+		vcall!(self, getDefinedEntryPointCount())
+	}
+
+	pub fn get_defined_entry_point(&self, index: SlangInt32) -> Option<EntryPoint> {
+		let mut entry_point = null_mut();
+		vcall!(self, getDefinedEntryPoint(index, &mut entry_point));
 		Some(EntryPoint(IUnknown(std::ptr::NonNull::new(
 			entry_point as *mut _,
 		)?)))
