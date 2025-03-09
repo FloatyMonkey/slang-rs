6008af5d8ec839c9bb57e0c460ff458df404b761
Lauro Oyen <laurooyen@gmail.com>
2025-03-09 10:31:02 +0100

    Format code

--- src/lib.rs
+++ src/lib.rs
@@ -505,10 +505,8 @@ impl Module {
 		)?)))
 	}
 
-	pub fn entry_points(&self) ->  impl ExactSizeIterator<Item = EntryPoint> {
-		(0..self.entry_point_count())
-			.map(move |i| self.entry_point_by_index(i).unwrap())
-
+	pub fn entry_points(&self) -> impl ExactSizeIterator<Item = EntryPoint> {
+		(0..self.entry_point_count()).map(move |i| self.entry_point_by_index(i).unwrap())
 	}
 
 	pub fn name(&self) -> &str {
@@ -526,19 +524,17 @@ impl Module {
 		unsafe { CStr::from_ptr(identity).to_str().unwrap() }
 	}
 
-    pub fn dependency_file_count(&self) -> i32 {
-        vcall!(self, getDependencyFileCount()) as i32
-    }
+	pub fn dependency_file_count(&self) -> i32 {
+		vcall!(self, getDependencyFileCount()) as i32
+	}
 
-    pub fn dependency_file_path(&self, index: i32) -> &str {
-        let path = vcall!(self, getDependencyFilePath(index as i32));
-        unsafe { CStr::from_ptr(path).to_str().unwrap() }
-    }
+	pub fn dependency_file_path(&self, index: i32) -> &str {
+		let path = vcall!(self, getDependencyFilePath(index as i32));
+		unsafe { CStr::from_ptr(path).to_str().unwrap() }
+	}
 
-	pub fn dependency_file_paths(&self) ->  impl ExactSizeIterator<Item = &str> {
-		(0..self.dependency_file_count())
-			.map(move |i| self.dependency_file_path(i))
-
+	pub fn dependency_file_paths(&self) -> impl ExactSizeIterator<Item = &str> {
+		(0..self.dependency_file_count()).map(move |i| self.dependency_file_path(i))
 	}
 
 	pub fn module_reflection(&self) -> &reflection::Decl {
